2ea4ebae665037347873a88a5da633d2ab740b98
管理者 <kanrisha@kanrishanoMacBook-Pro.local>
2026-04-14 13:18:24 +0900

    Content-TypeをText/plain;charset=utf-8に変更

--- src/App.jsx
+++ src/App.jsx
@@ -289,7 +289,7 @@ export default function App() {
                   await fetch('https://script.google.com/macros/s/AKfycbxTf-VBKyqHIwzm2rH-SILfN0KQ4iMUYAfX0XoWKu_6t6MslcFUR63LwetGNuFWRw8S/exec',{
                     method:'POST',
                     mode:'no-cors',
-                    headers:{'Content-Type':'text/plain'},
+                    headers:{'Content-Type':'text/plain;charset=utf-8'},
                     body:(()=>{
                       const scRows=paySchedule();
                       let scheduleStr='';
